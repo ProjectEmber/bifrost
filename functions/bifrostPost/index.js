54185997bc0ea5b162f67923c537faf67f5133cc
Alessio Moretti <alessio.moretti@live.it>
2017-04-12 13:06:44 +0200

    Add skeletons for functions

---
+++ functions/bifrostPost/index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// including AWS SDK
+const AWS = require('aws-sdk');
+// including Elasticsearch SDK
+const Elasticsearch = require('elasticsearch');
+
+// retrieving handlers
+exports.handler = (event, context, callback) => {
+
+    // done callback implementing API protocol 
+    const done = (err, res) => callback(null, {
+        version: event.version,
+        code: err ? '0' : '1',
+        msg: err ? err.message : res,
+        data: null,
+    });
+
+
+}
