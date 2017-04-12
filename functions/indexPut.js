35974bc7d453c796560ebf31cb7cb395282e2b59
Alessio Moretti <alessio.moretti@live.it>
2017-04-12 23:26:30 +0200

    Add lambda function to put a new lamp with coordinates  support for geoquery - we are assuming that the name is the address of the cu location

--- functions/indexPut.js
+++ functions/indexPut.js
@@ -22,7 +22,40 @@ exports.handler = (event, context, callback) => {
         host: 'db.project-ember.city:9200',
         log: 'trace'
     });
-
     
+    // parsing parameters sent via post
+    var new_cu = null;
+    try {
+        new_cu = {
+            id:       event.id,
+            body : {
+                name:     event.name,
+                ip:       event.ip,
+                location: [
+                    event.location.longitude,
+                    event.location.latitude
+                ]
+            }
+        };
+    } catch (error) {
+        done(new Error("Error parsing parameters"), null);
+    }
 
+    // performing index operation
+    client.index(
+        {
+            index: 'ember',
+            type:  'control_unit',
+            id:    new_cu.id,
+            body:  new_cu.body
+        }
+    ).then(function(err) {
+        // returning error if any
+        if (err) 
+            done(new Error(err.message), null);
+        // returning true if not
+        else
+            done(null, true)
+    });
+    
 }
